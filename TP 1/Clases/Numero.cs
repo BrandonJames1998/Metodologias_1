08a841dd822288bcaa24fed1d40eed03c760c83e
Franco Díaz <francogermandiaz.1998@gmail.com>
2025-08-26 13:43:14 -0300

    Primer Commit

---
+++ TP 1/Clases/Numero.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TP_1
+{
+	public class Numero : Comparable
+	{
+		private int valor;
+		
+		public Numero(int num)
+		{
+			this.valor = num;
+		}
+		
+		public void getValor()
+		{
+			return valor;
+		}
+		
+		public bool sosIgual(Comparable c)
+		{
+			Numero n = ((Numero)c).getValor();
+			return this.valor == n.getValor();
+		}
+		public bool sosMenor(Comparable c)
+		{
+			Numero n = ((Numero)c).getValor();
+			return this.valor < n.getValor();
+		}
+		public bool sosMayor(Comparable c)
+		{
+			Numero n = ((Numero)c).getValor();
+			return this.valor > n.getValor();
+		}
+	}
+}
---
+++ TP 1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+namespace TP_1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Coleccionable pila = new Pila();
+			Coleccionable cola = new cola();
+			
+			
+			Console.ReadKey(true);
+		}
+		
+		static void Llenar(Coleccionable coleccion) 
+		{
+        	Random rnd = new Random();
+        	for (int i = 0; i < 20; i++) 
+        	{
+            	int valor = rnd.Next(1, 101); // número aleatorio entre 1 y 100
+            	Comparable c = new Numero(valor);
+            	coleccion.agregar(c);
+        	}
+		}
+		
+		static void Informar(Coleccionable coleccion)
+		{
+			Console.WriteLine("Cantidad de elementos: {0}", coleccion.cuantos());
+			Console.WriteLine("Elemento mínimo: {0}", coleccion.minimo());
+			Console.WriteLine("Elemento máximo: {0}", coleccion.maximo());
+			Console.Write("Ingrese un número a buscar: ");
+    		int valor = int.Parse(Console.ReadLine());
+    		Comparable buscado = new Numero(valor);
+    		if (coleccion.contiene(valor))
+    		{
+    			Console.WriteLine("El elemento está en la colección.");
+    		}
+    		else 
+    			Console.WriteLine("El elemento no se encuentra en la colección.");
+		}
+	}
+}
